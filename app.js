fafe20c7a66a763e3dc1e8ca797a5f58190fdb35
hamza-asif22 <hamzaasifbaig@gmail.com>
2023-12-29 20:57:24 +0500

    Mongo

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require("mongoose");
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/api/users');
-var productsRouter = require('./routes/api/products');
-var config = require("config");
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
@@ -21,8 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/products', productsRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,17 +36,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-
-mongoose.connect(config.get("db"),{useNewUrlParser: true,useUnifiedTopology: true})
-.then(()=> console.log("Connected to Mongo...."))
-.catch((error)=> console.log(error.message));
-
-
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-  console.log('Server is running on port ${PORT}');
 });
 
 module.exports = app;
